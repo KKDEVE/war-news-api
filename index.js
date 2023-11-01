b138d7673b3d01830af5c72e6c3ea5c1b1f79bce
KKDEVE <116938392+Codedkk@users.noreply.github.com>
2023-11-01 19:33:53 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,122 @@
+const PORT = process.env.PORT || 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const app = express()
+
+const newspapers = [
+    {
+        name: 'cityam',
+        address: 'https://www.cityam.com/category/politics/',
+        base: ''
+    },
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/#section-news',
+        base: ''
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/international',
+        base: '',
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/world-news/',
+        base: 'https://www.telegraph.co.uk',
+    },
+    {
+        name: 'nyt',
+        address: 'https://www.nytimes.com/section/world',
+        base: '',
+    },
+    {
+        name: 'latimes',
+        address: 'https://www.latimes.com/environment',
+        base: '',
+    },
+    {
+        name: 'smh',
+        address: 'https://www.latimes.com/world-nation',
+        base: 'https://www.smh.com.au',
+    },
+    {
+        name: 'bbc',
+        address: 'https://www.bbc.com/news/world',
+        base: 'https://www.bbc.co.uk',
+    },
+    {
+        name: 'es',
+        address: 'https://www.standard.co.uk/news/world',
+        base: 'https://www.standard.co.uk'
+    },
+    {
+        name: 'sun',
+        address: 'https://www.thesun.co.uk/news/politics/',
+        base: ''
+    },
+    {
+        name: 'dm',
+        address: 'https://www.dailymail.co.uk/home/index.html',
+        base: ''
+    }
+
+]
+
+const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("war ")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+
+        })
+})
+
+app.get('/', (req, res) => {
+    res.json('Welcome to War News API')
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("war ")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
